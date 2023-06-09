c0284adc608418dd0fca77737370b682801c185c
Kotaro-Miyagi <miaygi.bad@icloud.com>
2023-06-09 11:54:15 +0900

    README修正3
    Co-authored-by: Kotaro-Miyagi <kotaro-miyagi@users.noreply.github.com>

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const setupServer = require("../server/src/routes/index");
+// const chai = require("chai");
+// const chaiHttp = require("chai-http");
+// const setupServer = require("../server/src/routes/index");
