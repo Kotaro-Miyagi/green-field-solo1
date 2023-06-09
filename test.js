6d619c68e38cc5bbca4c074b888ab8b48c15d886
Kotaro-Miyagi <miaygi.bad@icloud.com>
2023-06-09 11:35:21 +0900

    デプロイ中2
    Co-authored-by: Kotaro-Miyagi <kotaro-miyagi@users.noreply.github.com>

--- test.js
+++ test.js
@@ -1,25 +1,3 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const setupServer = require("../server/src/routes/index");
-
-chai.use(chaiHttp);
-
-const expect = chai.expect;
-let request;
-
-const server = setupServer();
-
-describe("GET:cssAlldata", () => {
-  beforeEach(() => {
-    request = chai.request("http://localhost:3333");
-  });
-
-  it("it should return 200", async () => {
-    request.get("/api/data").end((err, res) => {
-      // console.log(res.statusCode);
-
-      expect(res).to.have.status(200);
-      expect(err).to.be.null;
-    });
-  });
-});
